315332177a69877e2e632c28f04456ff9cfceba1
catg2265 <cgrave20@student.aau.dk>
2024-11-09 19:39:36 +0100

    Update GameManager.cs

--- CreateJameFall2024Project/Assets/Scripts/GameManager.cs
+++ CreateJameFall2024Project/Assets/Scripts/GameManager.cs
@@ -19,11 +19,13 @@ public class GameManager : MonoBehaviour
         ghostList.Add(new Ghost("Angry robust old man, always has a bottle in his hand. \r\nHe has a rosacea, a loud voice, and smells strong. He coughs a lot.\r\nWorked as a miner for many years, until his health made it impossible for him to continue working. \r\nNeighbors say that sometimes he can become very violent towards his wife. \r\nDied of alcoholism.", false, false, false, false, true, false, false));
         //If a characteristic equals 1 call that true, if it equals 0 call that false
         ghostList.Add(new Ghost("Insert text here inside citation", false, false, false, false, true, false, false));
+        //Please add the rest of the ghosts
+
 
         ghoulList.Add(new Ghoul(new GhoulHint[] { new GhoulHint("Rage is my inner beast.", new int[] {1,7,9 }), new GhoulHint("My back hurts a lot these days, I think I am getting old.", new int[] { 1,5,9}), new GhoulHint("La la la, la la la!", new int[] { 1,2,3,4,5,6,7,8,9}) }));
         //if more Ghoulhints are needed, add new GhoulHint("", new int[] {})
         ghoulList.Add(new Ghoul(new GhoulHint[] { new GhoulHint("Insert Hint in citation", new int[] {}), new GhoulHint("", new int[] {}), new GhoulHint("", new int[] { }) /* add extra ghoul hint here if needed*/ }));
-
+        //Please add the rest of the "Ghouls"
 
 
 
