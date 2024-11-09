a37c09af2719ecb886b623f56add7615af928ed1
catg2265 <cgrave20@student.aau.dk>
2024-11-09 19:08:49 +0100

    Update GameManager.cs

--- CreateJameFall2024Project/Assets/Scripts/GameManager.cs
+++ CreateJameFall2024Project/Assets/Scripts/GameManager.cs
@@ -1,6 +1,82 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public string ghoulIntroduction;
+
+    public List<Ghost> ghostList = new List<Ghost>();
+    public List<Ghoul> ghoulList = new List<Ghoul>();
+
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+}
+public class Ghost
+{
+    string summary;
+    bool intelligence;
+    bool love;
+    bool luck;
+    bool beauty;
+    bool power;
+    bool wealth;
+    bool health;
+    public Ghost(string summary, bool intelligence, bool love, bool luck, bool beauty, bool power, bool wealth, bool health)
+    {
+        this.summary = summary;
+        this.intelligence = intelligence;
+        this.love = love;
+        this.luck = luck;
+        this.beauty = beauty;
+        this.power = power;
+        this.wealth = wealth;
+        this.health = health;
+    }
+}
+public class Ghoul
+{
+    public GhoulHint[] ghoulHints;
+    public Ghoul(GhoulHint hint1, GhoulHint hint2, GhoulHint hint3)
+    {
+        ghoulHints = new GhoulHint[3] { hint1, hint2, hint3};
+    }
+    public int GhostMatch(int GhostID)
+    {
+        int totalMatch = 0;
+
+        foreach (GhoulHint ghoulHint in ghoulHints)
+        {
+            foreach (int id in ghoulHint.ghostMatchIds)
+            {
+                if (id == GhostID)
+                {
+                    totalMatch++; 
+                }
+            }
+        }
+
+        return totalMatch;
+    }
+}
+public class GhoulHint
+{
+    public string hint;
+    public int[] ghostMatchIds;
+    public GhoulHint(string hint, int[] ghostMatchIds)
+    {
+        this.hint = hint;
+        this.ghostMatchIds = ghostMatchIds;
+    }
 }
