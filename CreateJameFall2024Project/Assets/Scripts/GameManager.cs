ac2c6eff0be87f6a80067e8ca6f2c13a57a38656
catg2265 <cgrave20@student.aau.dk>
2024-11-09 19:47:10 +0100

    Update GameManager.cs

--- CreateJameFall2024Project/Assets/Scripts/GameManager.cs
+++ CreateJameFall2024Project/Assets/Scripts/GameManager.cs
@@ -21,10 +21,18 @@ public class GameManager : MonoBehaviour
         ghostList.Add(new Ghost("Insert text here inside citation", false, false, false, false, true, false, false));
         //Please add the rest of the ghosts
 
+        GhoulHint A = new GhoulHint("Don't be afraid of being eaten, love will accompany you.", new int[] {2,4,8});
+        GhoulHint B = new GhoulHint("Rage is my inner beast.", new int[] { 1, 7, 9 });
+        GhoulHint C = new GhoulHint("Do you believe in the power of faith?", new int[] {5,7,8 });
+        GhoulHint D = new GhoulHint("Look, I am so blue.", new int[] {3,6,7});
+        GhoulHint E = new GhoulHint("I forget my umbrella everytime when it rains.", new int[] {2,3,8});
+        GhoulHint F = new GhoulHint("My back hurts a lot these days, I think I am getting old.", new int[] { 1, 5, 9 });
+        GhoulHint G = new GhoulHint("You may not believe, but most ghouls die young.", new int[] {2,4,6});
+        GhoulHint H = new GhoulHint("I am a grown-up now, but sometimes I wonder, how does it feels to be...a female ghoul?", new int[] {5,8,9});
+        GhoulHint I = new GhoulHint("To have a big brain is not always a blessing...but it's still nice to have it.", new int[] {5,6,7});
+        GhoulHint J = new GhoulHint("La la la, la la la!", new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 
-        ghoulList.Add(new Ghoul(new GhoulHint[] { new GhoulHint("Rage is my inner beast.", new int[] {1,7,9 }), new GhoulHint("My back hurts a lot these days, I think I am getting old.", new int[] { 1,5,9}), new GhoulHint("La la la, la la la!", new int[] { 1,2,3,4,5,6,7,8,9}) }));
-        //if more Ghoulhints are needed, add new GhoulHint("", new int[] {})
-        ghoulList.Add(new Ghoul(new GhoulHint[] { new GhoulHint("Insert Hint in citation", new int[] {}), new GhoulHint("", new int[] {}), new GhoulHint("", new int[] { }) /* add extra ghoul hint here if needed*/ }));
+        ghoulList.Add(new Ghoul(new GhoulHint[] {  B,F,J  }));
         //Please add the rest of the "Ghouls"
 
 
